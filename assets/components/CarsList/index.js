3d5be530b5ffac2bcf1da9e4b5d64f42663e48c4
akashtech-source <mr.ifranakash@gmail.com>
2021-06-06 04:45:34 +0600

    car list done

---
+++ assets/components/CarsList/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, FlatList} from 'react-native';
+import styles from './styles'
+import CarItem from '../CarItem';
+
+import cars from './cars';
+
+const CarsList = (props) => {
+    return(
+        <View style={styles.container}>
+            <FlatList 
+            data={cars}
+            renderItem={({item}) => <CarItem car={item} />}
+            />
+        </View>
+    );
+};
+
+export default CarsList;
